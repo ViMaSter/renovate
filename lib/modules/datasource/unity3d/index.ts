d463171562667ef0de3245f17d6511b0ed5bb3c0
Vincent Mahnke <vincent@mahn.ke>
2024-02-11 03:43:35 +0100

    feat: Adds Unity3d datasource, versioning + tests

---
+++ lib/modules/datasource/unity3d/index.ts
@@ -0,0 +1,86 @@
+import { XmlDocument } from 'xmldoc';
+import { cache } from '../../../util/cache/package/decorator';
+import * as Unity3dVersioning from '../../versioning/unity3d';
+import { Datasource } from '../datasource';
+import type { GetReleasesConfig, Release, ReleaseResult } from '../types';
+
+export class Unity3dDatasource extends Datasource {
+  static readonly streams: { [key: string]: string } = {
+    lts: 'https://unity.com/releases/editor/lts-releases.xml',
+    stable: 'https://unity.com/releases/editor/releases.xml',
+    beta: 'https://unity.com/releases/editor/beta/latest.xml',
+  };
+
+  static readonly id = 'unity3d';
+
+  override readonly defaultRegistryUrls = [
+    Unity3dDatasource.streams.stable,
+    Unity3dDatasource.streams.beta,
+  ];
+
+  override readonly defaultVersioning = Unity3dVersioning.id;
+
+  override readonly registryStrategy = 'hunt';
+
+  constructor() {
+    super(Unity3dDatasource.id);
+  }
+
+  getByStream = async (
+    registryUrl: string | undefined,
+    withHash: boolean,
+  ): Promise<ReleaseResult | null> => {
+    const registryUrls = registryUrl
+      ? [registryUrl]
+      : [Unity3dDatasource.streams.stable, Unity3dDatasource.streams.beta];
+
+    const responses = await Promise.all(
+      registryUrls.map((url) => this.http.get(url)),
+    );
+
+    const releases: Release[] = responses
+      .map((response) => {
+        const document = new XmlDocument(response.body);
+
+        return (
+          document
+            ?.childNamed('channel')
+            ?.childrenNamed('item')
+            .map((itemNode) => {
+              const versionWithHash =
+                `${itemNode.childNamed('title')?.val} (${itemNode.childNamed('guid')?.val})`!;
+              const versionWithoutHash = itemNode.childNamed('title')?.val;
+              const release: Release = {
+                version: withHash ? versionWithHash : versionWithoutHash!,
+                releaseTimestamp: itemNode.childNamed('pubDate')?.val ?? '',
+                changelogUrl: itemNode.childNamed('link')?.val ?? '',
+                isStable: registryUrl !== Unity3dDatasource.streams.beta,
+                registryUrl,
+              };
+              return release;
+            }) ?? []
+        );
+      })
+      .flat();
+
+    return {
+      releases,
+      homepage: 'https://unity.com/',
+    };
+  };
+
+  @cache({
+    namespace: `datasource-${Unity3dDatasource.id}`,
+    key: (url: string) => url,
+    ttlMinutes: 24 * 60,
+  })
+  async getReleases({
+    packageName,
+    registryUrl,
+  }: GetReleasesConfig): Promise<ReleaseResult | null> {
+    return await this.getByStream(
+      registryUrl,
+      packageName === 'm_EditorVersionWithRevision',
+    );
+  }
+}
---
+++ lib/modules/versioning/unity3d/index.ts
@@ -0,0 +1,84 @@
+import semver from 'semver';
+import { regEx } from '../../../util/regex';
+import { GenericVersion, GenericVersioningApi } from '../generic';
+import type { VersioningApi } from '../types';
+
+export const id = 'Unity3d';
+export const displayName = 'Unity3d';
+export const urls = [
+  'https://docs.unity3d.com/Manual/ScriptCompilationAssemblyDefinitionFiles.html#version-define-expressions:~:text=characters%20are%20supported.-,Unity%20version%20numbers,-Current%20versions%20of',
+];
+export const supportsRanges = false;
+
+class Unity3dVersioningApi extends GenericVersioningApi {
+  private static readonly ReleaseStreamType = new Map([
+    ['Alpha', 'a'],
+    ['Beta', 'b'],
+    ['Patch', 'p'],
+    ['Experimental', 'x'],
+    ['Stable', 'f'],
+    ['Stable (China)', 'c'],
+  ]);
+  private static readonly ReleaseStreamTypeKeyOrder = Array.from(
+    Unity3dVersioningApi.ReleaseStreamType.keys(),
+  );
+
+  protected _parse(version: string): GenericVersion | null {
+    const matches = regEx(
+      /^(?<Major>\d+)\.(?<Minor>\d+)\.(?<Patch>\d+)(?<ReleaseStream>\w)(?<Build>\d+)/gm,
+    ).exec(version);
+    if (!matches) {
+      return null;
+    }
+    const Major = matches.groups?.Major ?? '';
+    const Minor = matches.groups?.Minor ?? '';
+    const Patch = matches.groups?.Patch ?? '';
+    const ReleaseStream = matches.groups?.ReleaseStream ?? '';
+    const Build = matches.groups?.Build ?? '';
+
+    const release = [
+      parseInt(Major),
+      parseInt(Minor),
+      parseInt(Patch) * 1000 + parseInt(Build),
+    ];
+    const stable = [
+      Unity3dVersioningApi.ReleaseStreamType.get('Stable'),
+      Unity3dVersioningApi.ReleaseStreamType.get('Stable (China)'),
+    ].includes(ReleaseStream);
+
+    let suffix = '';
+    if (version.match(/\([a-f0-9]+\)$/)) {
+      suffix = version.substring(1).substring(version.length - 2);
+    }
+    return { release, prerelease: stable ? undefined : Build, suffix };
+  }
+
+  protected override _compare(lhs: string, rhs: string): number {
+    const semverLhs = semver.parse(lhs.split(/(?=[a-z])/)[0]);
+    const semverRhs = semver.parse(rhs.split(/(?=[a-z])/)[0]);
+
+    const releaseStreamLhs = lhs.match(/(?<=[a-z])/)![0];
+    const releaseStreamRhs = rhs.match(/(?<=[a-z])/)![0];
+
+    const indexLhs =
+      Unity3dVersioningApi.ReleaseStreamTypeKeyOrder.indexOf(releaseStreamLhs);
+    const indexRhs =
+      Unity3dVersioningApi.ReleaseStreamTypeKeyOrder.indexOf(releaseStreamRhs);
+
+    if (indexLhs > indexRhs) {
+      return 1;
+    }
+    if (indexLhs < indexRhs) {
+      return -1;
+    }
+
+    if (!(semverLhs && semverRhs)) {
+      return super._compare(lhs, rhs);
+    }
+    return semverLhs.compare(semverRhs);
+  }
+}
+
+export const api: VersioningApi = new Unity3dVersioningApi();
+
+export default api;
