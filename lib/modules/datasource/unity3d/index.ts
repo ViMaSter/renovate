8ac7006b5c2080b6e03cd17b2ea09550119714d9
Vincent <vincent@mahn.ke>
2024-02-13 02:29:38 +0100

    fix: Apply linting

--- lib/modules/datasource/unity3d/index.ts
+++ lib/modules/datasource/unity3d/index.ts
@@ -46,8 +46,7 @@ export class Unity3dDatasource extends Datasource {
           ?.childNamed('channel')
           ?.childrenNamed('item')
           .map((itemNode) => {
-            const versionWithHash =
-              `${itemNode.childNamed('title')?.val} (${itemNode.childNamed('guid')?.val})`;
+            const versionWithHash = `${itemNode.childNamed('title')?.val} (${itemNode.childNamed('guid')?.val})`;
             const versionWithoutHash = itemNode.childNamed('title')?.val;
             const release: Release = {
               version: withHash ? versionWithHash : versionWithoutHash!,
